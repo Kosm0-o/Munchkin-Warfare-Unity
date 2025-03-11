c0c03099f06c878ae1c6176195cc5177086da54a
Kosm0-o <logantreboutat@gmail.com>
2025-03-11 13:42:43 -0700

    Update vILLAGE.cs

--- vILLAGE.cs
+++ vILLAGE.cs
@@ -6,8 +6,10 @@ public class vILLAGE : MonoBehaviour
 {
 
     public GameObject workerPrefab;
+    // Transform variable are positions, size, or rotations, usually
     public Transform spawnPoint;
     public float timeBetweenSpawns;
+    // notice that this is a dependent variable, therefore not public (not accessible in unity)
     float nextSpawnTime;
 
     public int numberOfWorkersToSpawn;
@@ -21,6 +23,7 @@ public class vILLAGE : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // same system as worker script for resource collection
         if (Time.time > nextSpawnTime) {
             nextSpawnTime = Time.time + timeBetweenSpawns;
             Instantiate(workerPrefab, spawnPoint.position, Quaternion.identity);

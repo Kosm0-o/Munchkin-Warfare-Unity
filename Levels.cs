d3e91c717ee9fb67ba2cf9486883fef63c3de6ea
Kosm0-o <logantreboutat@gmail.com>
2025-03-20 14:04:47 -0700

    Update Levels.cs

--- Levels.cs
+++ Levels.cs
@@ -9,9 +9,11 @@ public class Levels : MonoBehaviour
     /*
     Level 1: Intro (Basic Mechanics, Slow Enemies)
     Level 2: Enemy Upgrade (A Little Bit Faster Normal Enemies, New Slow/Fat Enemies with More Health)
-    Level 3: 
+    Level 3: Enemy Upgrade (Normal Enemies are Really Fast)
+    Level 4: Difficulty Upgrade (Big Time Goal Leap/Sacrifice Goal Leap
+    Level 5: Insane 
     */
-    public int[] levelTime = new int[] {120, 240, };
+    public int[] levelTime = new int[] {120, 240, 480, 1250};
 
     // Start is called before the first frame update
     void Start()

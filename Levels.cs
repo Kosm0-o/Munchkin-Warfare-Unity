6ee4731114ba6e956cbc4a0996d9696fa0f7224e
Kosm0-o <logantreboutat@gmail.com>
2025-03-20 11:37:01 -0700

    Update Levels.cs

--- Levels.cs
+++ Levels.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Resource : MonoBehaviour
+public class Levels : MonoBehaviour
 {
+
+    public int level;
+    /*
+    Level 1: Intro (Basic Mechanics, Slow Enemies)
+    Level 2: Enemy Upgrade (A Little Bit Faster Normal Enemies, New Slow/Fat Enemies with More Health)
+    Level 3: 
+    */
+    public int[] levelTime = new int[] {120, 240, };
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if 
     }
 }

e28288796cb1a2d60e9d6118f5ad8cdb2617a372
Kosm0-o <logantreboutat@gmail.com>
2025-03-24 09:09:23 -0700

    Update Levels.cs

--- Levels.cs
+++ Levels.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Levels : MonoBehaviour
 {
 
-    public int level;
+    public int levelNum;
+    public string levelType;
+    public static Levels instance;
     /*
     Level 1: Intro (Basic Mechanics, Slow Enemies)
     Level 2: Enemy Upgrade (A Little Bit Faster Normal Enemies, New Slow/Fat Enemies with More Health)
@@ -13,17 +15,43 @@ public class Levels : MonoBehaviour
     Level 4: Difficulty Upgrade (Big Time Goal Leap/Sacrifice Goal Leap
     Level 5: Insane 
     */
-    public int[] levelTime = new int[] {120, 240, 480, 1250};
+    public int[] survivalTime = new int[] {120, 240, 480, 1250};
+    public int[] sacrificeGoal = new int[] {10, 25, 45, 55, 100};
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-        if 
+        if (levelType == survival) {
+            if (Time.time < survivalTime[levelNum]) {
+                levelNum ++;
+            }
+        } 
+        else if (levelType == sacrifice) {
+            if () {
+            
+            }
+        }
     }
+
+    private string LevelTypeChecker() 
+    {
+        if ()
+    }
+
+    private void levelUp() {
+    
+    }
+    
 }
